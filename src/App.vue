6ca19974f6c2de0fe4285b4304a51ec7ea8182b9
Joe Kausits <JoeKausits@gmail.com>
2017-12-06 00:01:44 -0500

    Output books from the database in a table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vue.js 2 & Firebase Sample Application</h1>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books List</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books" v-bind:key="book.id">
+              <td>
+                <a v-bind:href="book.url">{{book.title}}</a>
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,24 +52,26 @@ let config = {
 let app = Firebase.initializeApp(config);
 let db = app.database();
 
-let bookRef = db.ref('books');
+let booksRef = db.ref('Books');
 export default {
   name: 'app',
   firebase: {
     books: booksRef
   },
   components: {
+  },
+  data(){
+    return{
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
